6abe5b60d402d2378cedadc3f39592fd93286d19
seanfaubus <seanfaubus@gmail.com>
2022-05-08 13:34:05 -0500

    js

---
+++ script.js
@@ -0,0 +1,9 @@
+$(documentO).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+});
